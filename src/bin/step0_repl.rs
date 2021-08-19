5d3def3d755003c21e7705e9338bffc6fb113b1a
Atahan Yorgancı <atahanyorganci@hotmail.com>
2021-08-20 02:53:43 +0300

    Step0: Use String instead of &str

--- src/bin/step0_repl.rs
+++ src/bin/step0_repl.rs
@@ -1,18 +1,18 @@
 use rustyline::{error::ReadlineError, Editor};
 
-fn read(input: &str) -> &str {
+fn read(input: String) -> String {
     input
 }
 
-fn eval(input: &str) -> &str {
+fn eval(input: String) -> String {
     input
 }
 
-fn print(input: &str) -> &str {
+fn print(input: String) -> String {
     input
 }
 
-fn rep(input: &str) -> &str {
+fn rep(input: String) -> String {
     let ast = read(input);
     let result = eval(ast);
     print(result)
@@ -24,8 +24,8 @@ fn main() {
         let readline = editor.readline("user> ");
         match readline {
             Ok(line) => {
-                println!("{}", rep(line.trim_end()));
-                editor.add_history_entry(line);
+                editor.add_history_entry(&line);
+                println!("{}", rep(line));
             }
             Err(ReadlineError::Eof) => break,
             Err(ReadlineError::Interrupted) => break,
