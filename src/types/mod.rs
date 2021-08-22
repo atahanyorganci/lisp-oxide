ad06356c2fe5904be2d2f68d572ad08ed77d431e
Atahan Yorgancı <atahanyorganci@hotmail.com>
2021-08-22 13:13:58 +0300

    Implement `nil` type
    Implement `nil` for expressions that do not return a value. Nil type is
    zero-sized type that can be represeneted as a single pointer, since nil
    is unique for a given mal program.

--- src/types/mod.rs
+++ src/types/mod.rs
@@ -1,6 +1,7 @@
 use std::{
     any::Any,
     fmt::{Debug, Display},
+    rc::Rc,
 };
 
 pub mod func;
@@ -28,5 +29,35 @@ impl dyn MalType {
             Some(int) => Ok(int),
             None => Err(()),
         }
+    }
+}
+
+pub struct MalNil {}
+
+impl Debug for MalNil {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "nil")
+    }
+}
+
+impl Display for MalNil {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "nil")
+    }
+}
+
+impl MalType for MalNil {
+    fn as_any(&self) -> &dyn Any {
+        self
+    }
+
+    fn as_any_mut(&mut self) -> &mut dyn Any {
+        self
+    }
+}
+
+impl MalNil {
+    pub fn new() -> Rc<Self> {
+        Rc::new(Self {})
     }
 }
