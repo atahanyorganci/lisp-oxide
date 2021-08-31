8ee3a609566b619b5d28f5bb4d434ab39557ed2f
Atahan Yorgancı <atahanyorganci@hotmail.com>
2021-08-31 20:34:41 +0300

    Moved tests for builtin_func macro

---
+++ mal-derive/tests/test.rs
@@ -0,0 +1,13 @@
+#[test]
+fn builtin_func_test() {
+    let t = trybuild::TestCases::new();
+    t.pass("tests/builtin_func/01-parse.rs");
+    t.pass("tests/builtin_func/02-outer-fn.rs");
+    t.pass("tests/builtin_func/03-mal-type.rs");
+    t.pass("tests/builtin_func/04-rc-ref.rs");
+    t.pass("tests/builtin_func/05-array.rs");
+    t.pass("tests/builtin_func/06-mixed-array.rs");
+    t.pass("tests/builtin_func/07-env.rs");
+    t.pass("tests/builtin_func/08-attr.rs");
+    t.compile_fail("tests/builtin_func/09-attr-fail.rs")
+}
